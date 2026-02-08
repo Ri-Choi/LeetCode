554b9aa68d6b51f5a9eeacea0efa99e985a39326
choiri <80485020+Ri-Choi@users.noreply.github.com>
2026-02-08 19:46:00 +0900

    Time: 1 ms (87.14%), Space: 57.2 MB (43.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,13 +1,13 @@
 function twoSum(nums: number[], target: number): number[] {
-    const result = [];
+    const map = new Map();
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i+1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                result.push(i,j)
-            }
+    for(let i = 0; i < nums.length; i++) {
+        const need = target - nums[i];
+        if(map.has(need)) {
+            return [map.get(need), i]
         }
+        
+        map.set(nums[i], i)
     }
 
-    return result;
 };
